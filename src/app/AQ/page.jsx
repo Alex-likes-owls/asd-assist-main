5492acad152cda3ca0dd7cd1c3accdaed1e740ec
Alex <saplingsmiles@gmail.com>
2025-03-11 22:48:56 +0800

    Fixed the input thing, need to add the logic for the scores

--- src/app/AQ/page.jsx
+++ src/app/AQ/page.jsx
@@ -1,21 +1,45 @@
+"use client";
+
+import { useState } from "react";
 import aqData from "../lib/testData";
 
 export default function AQ() {
+  const [object, setObject] = useState({});
   return (
-    <>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
       <p>The AQ-10</p>
       <div>
         {aqData.map((val, ind) => {
           return (
             <div key={ind}>
-              <p>{val.question}</p>
+              <p>
+                {ind + 1}. {val.question}
+              </p>
               {val.choices.map((v, i) => {
-                return <p key={i}>{v.answer}</p>;
+                return (
+                  <div key={i}>
+                    <input
+                      type="radio"
+                      name={val.question}
+                      value={v.score}
+                      id={i}
+                      onChange={prev => {
+                        
+                      }}
+                    />
+                    <label htmlFor="i">{v.answer}</label>
+                  </div>
+                );
               })}
+              <button type="submit">Score</button>
             </div>
           );
         })}
       </div>
-    </>
+    </form>
   );
 }
