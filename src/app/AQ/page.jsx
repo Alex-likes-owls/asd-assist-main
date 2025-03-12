cb36eccc3ab214dcfc8ae5dd178e7d09443aa56e
Alex <saplingsmiles@gmail.com>
2025-03-12 23:43:16 +0800

    Finished the logic, now I have to just make a hidden div box for the score

--- src/app/AQ/page.jsx
+++ src/app/AQ/page.jsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import aqData from "../lib/testData";
 
 export default function AQ() {
   const [object, setObject] = useState({});
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        let sum = 0;
+        let myArr = Object.values(object);
+        myArr.forEach((element) => {
+          sum += element;
+        });
+        if (myArr.length !== 10) {
+          alert("The test has not been finished");
+        }
+        console.log(sum);
       }}
     >
       <p>The AQ-10</p>
@@ -27,18 +37,22 @@ export default function AQ() {
                       name={val.question}
                       value={v.score}
                       id={i}
-                      onChange={prev => {
-                        
+                      onChange={() => {
+                        setObject((prev) => {
+                          let newObj = { ...prev };
+                          newObj[`question-${ind}`] = v.score;
+                          return newObj;
+                        });
                       }}
                     />
                     <label htmlFor="i">{v.answer}</label>
                   </div>
                 );
               })}
-              <button type="submit">Score</button>
             </div>
           );
         })}
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
